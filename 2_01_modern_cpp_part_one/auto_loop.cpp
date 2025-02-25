451dfe5c04cbbfb15170925705a5023dd49b6514
moezmefteh <moezmefteh85@gmail.com>
2025-02-25 17:13:36 +0100

    add comment to understand the auto utility

--- 2_01_modern_cpp_part_one/auto_loop.cpp
+++ 2_01_modern_cpp_part_one/auto_loop.cpp
@@ -4,7 +4,8 @@
 int main()
 {
 	std::vector<int> vec{4, 2, 3, 5, 1};
-	
+	//std::vector<std::string>::iterator it = vec.begin();
+	//auto it = vec.begin();
 	for (auto it = vec.begin(); it != vec.end(); ++it)
 		*it += 2;
 	
